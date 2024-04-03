d8acbe43aebd6370e436959153f150d1485a99d1
Nitin Kumawat <135094897+NITINKUMAWAT0@users.noreply.github.com>
2024-04-03 20:36:32 +0530

    made connections between FrontEnd and BackEnd

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,62 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios";
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      title: "Spider-Man: The Hanged Man",
-      img1: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1711180387_3878384.jpg?format=webp&w=480&dpr=1.3",
-      img2: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1711180387_8376898.jpg?format=webp&w=480&dpr=1.3",
-      isnew: true,
-      oldprice: "₹ 999",
-      price: "₹ 799",
-    },
-    {
-      id: 2,
-      title: "TSS Original: God Of Death",
-      img1: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1711186054_1884723.jpg?format=webp&w=480&dpr=1.3",
-      img2: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1711186054_2211944.jpg?format=webp&w=480&dpr=1.3",
-      isnew: true,
-      oldprice: "₹ 999",
-      price: "₹ 799",
-    },
-    {
-      id: 3,
-      title: "Dungeons & Dragons",
-      img1: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1710328305_3872574.jpg?format=webp&w=480&dpr=1.3",
-      img2: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1710328305_9728391.jpg?format=webp&w=480&dpr=1.3",
-      isnew: true,
-      oldprice: "₹ 999",
-      price: "₹ 799",
-    },
-    {
-      id: 4,
-      title: "Harry Potter: Death Eater",
-      img1: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1690348765_9479319.jpg?format=webp&w=480&dpr=1.3",
-      img2: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1690348765_3246618.jpg?format=webp&w=480&dpr=1.3",
-      isnew: true,
-      oldprice: "₹ 999",
-      price: "₹ 799",
-    },
-    {
-      id: 5,
-      title: "Kung Fu Panda: Warrior Business",
-      img1: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1710829195_2469715.jpg?format=webp&w=480&dpr=1.3",
-      img2: "https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1710567947_3761034.jpg?format=webp&w=480&dpr=1.3",
-      isnew: true,
-      oldprice: "₹ 999",
-      price: "₹ 799",
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_API_URL}/products`,
+          {
+            headers: {
+              Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN,
+            }
+          }
+        );
+        setData(res.data.data);
+        console.log(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} products</h1>
       </div>
       <div className="bottom">
-        {data.map((item) => (
+        {data && data.map((item) => (
           <Card item={item} key={item.id} />
         ))}
       </div>
