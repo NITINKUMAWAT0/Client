b7a93950fe19d2631968952c83a9d2e0d794cb45
Nitin Kumawat <135094897+NITINKUMAWAT0@users.noreply.github.com>
2024-04-25 13:13:33 +0530

    working on category page!!

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios";
 import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-  const { data, loading, error } = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`);
-
+ 
+  const {data,loading,error} = useFetch(`/products?populate=*&[filters][type][$eq]=${type}`)
+  
   return (
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} products</h1>
       </div>
       <div className="bottom">
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
+        {error ? (
           <p>Error: {error}</p>
-        ) : data && data.length > 0 ? (
-          data.map((item, index) => (
-            <Card item={item} key={index} />
-          ))
         ) : (
-          <p>No {type} products found.</p>
+          data.map((item) => (
+            <Card item={item} key={item.id} />
+          ))
         )}
       </div>
     </div>
