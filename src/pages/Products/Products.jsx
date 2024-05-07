be915315d8e04e07fb207ef62ee67e00cc583796
Nitin Kumawat <135094897+NITINKUMAWAT0@users.noreply.github.com>
2024-05-08 04:13:17 +0530

    working on products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,61 +2,82 @@ import React, { useState } from "react";
 import "./Products.scss";
 import Lists from "../../components/Lists/Lists";
 import { useParams } from "react-router-dom";
+import useFetch from "./../../hooks/useFetch";
 
 const Products = () => {
-  const catId = parseInt(useParams().id)
+  const catId = parseInt(useParams().id);
   const [maxPrice, setMaxPrice] = useState(1000);
-  const [sort, setSort] = useState(null)
+  const [sort, setSort] = useState(null);
+
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
+
+  const { data, loading, error } = useFetch(
+    `/sub-categories?[filter][categories][id][$eq]=${catId}`
+  );
   return (
     <div className="products">
       {/*left*/}
       <div className="left">
         <div className="filterItem">
-            <h2>Product Categories</h2>
-            <div className="inputItems">
-                <input type="checkbox" id="1" value={1} />
-                <label htmlFor="1">Shoes</label>
+          <h2>Product Categories</h2>
+          {data?.map((item) => (
+            <div className="inputItems" key={item.id}>
+              <input
+                type="checkbox"
+                id={item.id}
+                value={item.id}
+                onChange={handleChange}
+              />
+              <label htmlFor={item.id} className="abcd">
+                {item.attribute?.title}
+              </label>
             </div>
-            <div className="inputItems">
-                <input type="checkbox" id="2" value={2} />
-                <label htmlFor="2">Shirts</label>
-            </div>
-            <div className="inputItems">
-                <input type="checkbox" id="3" value={3} />
-                <label htmlFor="3">Over-sized Tshirts</label>
-            </div>
-            <div className="inputItems">
-                <input type="checkbox" id="4" value={4} />
-                <label htmlFor="4">Joggers</label>
-            </div>
+          ))}
         </div>
         <div className="filterItem">
-            <h2>Filter by price</h2>
-            <div className="inputItem">
-                <span>0</span>
-                <input type="range" min={0} max={2000} onChange={(e)=>setMaxPrice(e.target.value)}/>
-                <span>{maxPrice}</span>
-            </div>
-        </div>
-        <div className="filterItem">
-            <h2>Sort by</h2>
-            <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" onChange={e=>setSort('desc')}/>
-            <label htmlFor="asc">Price (Lowest first)</label>
-            </div>
-            <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" onChange={e=>setSort('asc')}/>
-            <label htmlFor="desc">Price (Highest  first)</label>
-            </div>
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
+            <label htmlFor="asc">Price (Highest first)</label>
+          </div>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
+            <label htmlFor="desc">Price (Lowest first)</label>
+          </div>
         </div>
       </div>
 
       {/*right*/}
       <div className="right">
-        <img className="catImg" 
-        src="https://prod-img.thesouledstore.com/public/theSoul/uploads/themes/9242120230327133914.jpg?format=webp&w=1500&dpr=1.3" 
-        alt="" />
-        <Lists catId={catId} maxPrice={maxPrice} sort={sort}/>
+        <img
+          className="catImg"
+          src=""
+          alt=""
+        />
+        <Lists catId={catId} maxPrice={maxPrice} sort={sort} SubCats={selectedSubCats} />
       </div>
     </div>
   );
